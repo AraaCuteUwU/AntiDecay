f62a013f981fb15580f226599fe95ac81ceb9169
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2022-06-27 09:20:59 +0800

    Add files via upload

--- src/FiraAja/AntiDecay/AntiDecay.php
+++ src/FiraAja/AntiDecay/AntiDecay.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace FiraAja\AntiDecay;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\LeavesDecayEvent;
+
+class AntiDecay extends PluginBase implements Listener {
+	
+	public function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onDecay(LeavesDecayEvent $event){
+		$event->cancel();
+	}
+}
