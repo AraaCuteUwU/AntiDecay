3d249cf38b14cbd3520400974e33dee0bfae86fd
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2023-05-03 22:50:31 +0800

    v2.0.0

--- src/FiraAja/AntiDecay/AntiDecay.php
+++ src/FiraAja/AntiDecay/AntiDecay.php
@@ -6,13 +6,28 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\block\LeavesDecayEvent;
 
-class AntiDecay extends PluginBase implements Listener {
-	
-	public function onEnable(): void {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
-	
-	public function onDecay(LeavesDecayEvent $event){
-		$event->cancel();
-	}
+class AntiDecay extends PluginBase implements Listener
+{
+
+    public function onEnable(): void
+    {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    /**
+     * @param LeavesDecayEvent $event
+     * @return void
+     */
+    public function onDecay(LeavesDecayEvent $event): void {
+        $block = $event->getBlock();
+        if (empty($this->getConfig()->get("worlds", [])))
+            $event->cancel();
+        else
+            foreach ($this->getConfig()->getAll() as $key) {
+                $from = $block->getPosition()->getWorld()->getFolderName();
+                if ($key == $from)
+                    $event->cancel();
+            }
+    }
 }
